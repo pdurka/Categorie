b3aea52704018d22ad38810fa3dd542290d9c710
pdurka <pawel.durka@westwing.pl>
2022-01-22 16:39:06 +0100

    deleting comments

--- app/Http/Controllers/CategorieController.php
+++ app/Http/Controllers/CategorieController.php
@@ -15,21 +15,6 @@ class CategorieController extends Controller
 
     public function store(Request $request)
     {
-//        $payload = [
-//            [
-//                'country' => 'PL',
-//                'name' => 'Wody',
-//            ],
-//            [
-//                'country' => 'EN',
-//                'name' => 'Water',
-//            ]
-//        ];
-//
-//        print_r( json_encode($payload) );
-//
-//        die;
-
         $categorie = Categories::create()->translations()->createMany($request->all());
 
         return response()->json($categorie, 201);
